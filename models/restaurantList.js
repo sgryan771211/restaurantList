4bfa6043ecdc4d1914f4d2d456cbf02553259279
dongdong <sgryan771211@gmail.com>
2019-04-20 14:23:45 +0800

    Add restaurantListSeeder and modify restaurantList.js

--- models/restaurantList.js
+++ models/restaurantList.js
@@ -4,7 +4,39 @@ const Schema = mongoose.Schema
 const restaurantListSchema = ({
   name: {
     type: String,
-    require: true,
+    required: true,
+  },
+  name_en: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  google_map: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
   }
 })
 
